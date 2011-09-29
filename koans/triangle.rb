1cb0cbf3836fd83e48bf8310603945f25bccdebd
Hank Gay <whg@us.ibm.com>
2011-09-29 11:39:59 -0400

    Implement sanity checking of triangle sides.
    This means that koans 154 and 155 now pass.

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,22 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  if a.nil? or b.nil? or c.nil?
+    raise TriangleError, "It can't be a triangle if it doesn't have three sides that actually exist."
+  end
+
+  if a <= 0 || b <= 0 || c <= 0
+    raise TriangleError, "The length of each triangle's side must be a natural number."
+  end
+
+  # Use a FP number so we don't accidentally round down to 0.
+  s = (a + b + c) / 2.0
+  # I totally forgot this property of triangles.
+  ok = ((s - a) * (s - b) * (s - c)) > 0
+  if !ok
+    raise TriangleError, "You violated that rule that I forgot."
+  end
+
   if a == b && b == c
     return :equilateral
   elsif a == b || a == c || b == c
@@ -22,6 +38,8 @@ def triangle(a, b, c)
     return :scalene
   end
 end
+
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
